f71c8ab66e37441768fdd7682e85d677070eee8b
irwantowibowotech <irwantowibowotech@gmail.com>
2021-01-26 09:54:29 +0700

    upload image (CRUD product)

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
@@ -6,6 +7,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -19,10 +21,19 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+/*
+Menjadikan folder upload mendaji static
+- hal ini kita lakukan supaya dapat diakses melalui frontend
+- defaultnya di heroku tidak bisa kita akses image dari sana, biasanya harus kita upload du clud (misal cloudinary atau amazon s3, dll)
+*/
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Custom error handler
 app.use(notFound);
