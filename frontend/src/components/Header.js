ba14219b11f3c8d0adc5d18af10e48082f3822f5
irwantowibowotech <irwantowibowotech@gmail.com>
2021-01-28 21:00:15 +0700

    create search product functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
+import { Route } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { logout } from "../redux/actions/userActions";
+import SearchBox from "./SearchBox";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -22,6 +24,7 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
+            <Route render={({ history }) => <SearchBox history={history} />} />
             <Nav className="ml-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
